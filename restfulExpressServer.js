2a875e32cd7dd34b3682446999bf2cdc4df57e95
Emily Ringoen <emilyaringoen@gmail.com>
2017-03-17 17:46:12 -0600

    fixed some bugs

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -26,38 +26,46 @@ app.get('/pets', (req, res) => {
   })
 })
 
-// app.get('/pets/:id', (req, res) => {
-//   fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-//     if (err) {
-//       console.error(err.stack);
-//       return res.sendStatus(404)
-//     }
-//     let id = Number.parseInt(req.params.id)
-//     let pets = JSON.parse(petsJSON)
-//     if (id < 0 || id >= pets.length || Number.isNaN(id)) {
-//       return res.sendStatus(404)
-//     }
-//     res.send(pets[id])
-//   })
-// })
-//
-// app.post('/pets', (req, res) => {
-//   let pet = req.body
-//   console.log(pet);
-//   fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-//     if (err) {
-//       console.error(err.stack);
-//       return res.sendStatus(404)
-//     }
-//     let pets = JSON.parse(petsJSON)
-//     pets.push(pet)
-//     res.send(pets)
-//     let JSONpet = JSON.stringify(pets)
-//     fs.writeFile('pets.json', JSONpet, (err) => {
-//       console.log(pet);
-//     })
-//   })
-// })
+app.get('/pets/:id', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(404)
+    }
+    let id = Number.parseInt(req.params.id)
+    let pets = JSON.parse(petsJSON)
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404)
+    }
+    res.send(pets[id])
+  })
+})
+
+app.post('/pets', (req, res) => {
+if (req.body.name === '' || req.body.age === '' || req.body.kind === '') {
+  res.statusCode = 400
+  res.setHeader('Content-Type', 'text/plain')
+  return res.end('Bad Request')
+}
+  let newPet = {
+    name: req.body.name,
+    age: req.body.age,
+    kind: req.body.kind
+  }
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(404)
+    }
+    let pets = JSON.parse(petsJSON)
+    pets.push(newPet)
+    res.send(newPet)
+    let JSONpet = JSON.stringify(pets)
+    fs.writeFile('pets.json', JSONpet, (err) => {
+      console.log(newPet);
+    })
+  })
+})
 
 app.patch('/pets/:id', (req, res) => {
   fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
@@ -65,40 +73,41 @@ app.patch('/pets/:id', (req, res) => {
       console.error(err.stack);
       return res.sendStatus(404)
     }
-    let pet = req.body
     let id = Number.parseInt(req.params.id)
     let pets = JSON.parse(petsJSON)
     if (id < 0 || id >= pets.length || Number.isNaN(id)) {
       return res.sendStatus(404)
     }
-    if (!pet) return res.sendStatus(400)
-    pets[id] = pet
-    res.send(pet)
+    for (let key in req.body) {
+      pets[id][key] = req.body[key]
+    }
+    res.send(pets[id])
     let JSONpet = JSON.stringify(pets)
     fs.writeFile('pets.json', JSONpet, (err) => {
-      console.log(pet);
+      console.log(pets[id]);
     })
   })
 })
 
-// app.delete('/pets/:id', (req, res) => {
-//   fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-//     if (err) {
-//       console.error(err.stack);
-//       return res.sendStatus(404)
-//     }
-//     let id = Number.parseInt(req.params.id)
-//     let pets = JSON.parse(petsJSON)
-//     if (id < 0 || id >= pets.length || Number.isNaN(id)) {
-//       return res.sendStatus(404)
-//     }
-//     let pet = pets.splice(id, 1)[0]
-//     res.send(pet)
-//     fs.writeFile('pets.json', JSONpet, (err) => {
-//       console.log(pets);
-//     })
-//   })
-// })
+app.delete('/pets/:id', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(404)
+    }
+    let id = Number.parseInt(req.params.id)
+    let pets = JSON.parse(petsJSON)
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404)
+    }
+    let pet = pets.splice(id, 1)[0]
+    res.send(pet)
+    let JSONpet = JSON.stringify(pets)
+    fs.writeFile('pets.json', JSONpet, (err) => {
+      console.log(pets);
+    })
+  })
+})
 
 
 
