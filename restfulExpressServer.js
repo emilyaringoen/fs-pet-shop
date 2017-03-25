0ebaa36db2852bc66cdce1a2e235d2aeabde3ddf
Emily Ringoen <emilyaringoen@gmail.com>
2017-03-25 15:46:13 -0600

    finished pet shop

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -1,24 +1,19 @@
-'use strict';
+'use strict'
+
 const http = require('http')
 const fs = require('fs')
 const express = require('express')
-const app = express()
-const path = require('path')
-const petsPath = path.join(__dirname, 'pets.json')
-const port = 8000
 const bodyParser = require('body-parser')
-
+const port = 8000
+const app = express()
 
 app.disable('x-powered-by')
 app.use(bodyParser.json())
 
-app.set('port', process.env.PORT || 8000)
-
-
 app.get('/pets', (req, res) => {
-  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+  fs.readFile('pets.json', 'utf8', (err, petsJSON) => {
     if (err) {
-      console.error(err.stack);
+      console.error(err.stack)
       return res.sendStatus(404)
     }
     let pets = JSON.parse(petsJSON)
@@ -27,53 +22,55 @@ app.get('/pets', (req, res) => {
 })
 
 app.get('/pets/:id', (req, res) => {
-  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+  fs.readFile('pets.json', 'utf8', (err, petsJSON) => {
     if (err) {
-      console.error(err.stack);
+      console.error(err.stack)
       return res.sendStatus(404)
     }
-    let id = Number.parseInt(req.params.id)
+    let id = +req.params.id
     let pets = JSON.parse(petsJSON)
+
     if (id < 0 || id >= pets.length || Number.isNaN(id)) {
       return res.sendStatus(404)
     }
+
     res.send(pets[id])
   })
 })
 
 app.post('/pets', (req, res) => {
-if (req.body.name === '' || req.body.age === '' || req.body.kind === '') {
-  res.statusCode = 400
-  res.setHeader('Content-Type', 'text/plain')
-  return res.end('Bad Request')
-}
-  let newPet = {
-    name: req.body.name,
-    age: req.body.age,
-    kind: req.body.kind
+  if (req.body.name === '' || req.body.kind === '' || req.body.age === '') {
+    res.statusCode = 400
+    res.setHeader('Content-Type', 'text/plain')
+    return res.end('Bad Request')
   }
-  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+  let newPet = {
+    age: req.body.age,
+    kind: req.body.kind,
+    name: req.body.name
+  }
+  fs.readFile('pets.json', 'utf8', (err, petsJSON) => {
     if (err) {
-      console.error(err.stack);
+      console.error(err.stack)
       return res.sendStatus(404)
     }
     let pets = JSON.parse(petsJSON)
     pets.push(newPet)
     res.send(newPet)
-    let JSONpet = JSON.stringify(pets)
-    fs.writeFile('pets.json', JSONpet, (err) => {
+    let stringPet = JSON.stringify(pets)
+    fs.writeFile('pets.json', stringPet, (err) => {
       console.log(newPet);
     })
   })
 })
 
 app.patch('/pets/:id', (req, res) => {
-  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+  fs.readFile('pets.json', 'utf8', (err, petsJSON) => {
     if (err) {
-      console.error(err.stack);
+      console.error(err.stack)
       return res.sendStatus(404)
     }
-    let id = Number.parseInt(req.params.id)
+    let id = +req.params.id
     let pets = JSON.parse(petsJSON)
     if (id < 0 || id >= pets.length || Number.isNaN(id)) {
       return res.sendStatus(404)
@@ -81,18 +78,18 @@ app.patch('/pets/:id', (req, res) => {
     for (let key in req.body) {
       pets[id][key] = req.body[key]
     }
+    let stringPet = JSON.stringify(pets)
     res.send(pets[id])
-    let JSONpet = JSON.stringify(pets)
-    fs.writeFile('pets.json', JSONpet, (err) => {
+    fs.writeFile('pets.json', stringPet, (err) => {
       console.log(pets[id]);
     })
   })
 })
 
 app.delete('/pets/:id', (req, res) => {
-  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+  fs.readFile('pets.json', 'utf8', (err, petsJSON) => {
     if (err) {
-      console.error(err.stack);
+      console.error(err.stack)
       return res.sendStatus(404)
     }
     let id = Number.parseInt(req.params.id)
@@ -100,23 +97,22 @@ app.delete('/pets/:id', (req, res) => {
     if (id < 0 || id >= pets.length || Number.isNaN(id)) {
       return res.sendStatus(404)
     }
-    let pet = pets.splice(id, 1)[0]
-    res.send(pet)
-    let JSONpet = JSON.stringify(pets)
-    fs.writeFile('pets.json', JSONpet, (err) => {
+    let gonePet = pets.splice(id, 1)[0]
+    res.send(gonePet)
+    let stringPet = JSON.stringify(pets)
+    fs.writeFile('pets.json', stringPet, (err) => {
       console.log(pets);
     })
   })
 })
 
 
-
 app.use((req, res) => {
   res.sendStatus(404);
 });
 
-app.listen(app.get('port'), () => {
-  console.log("listening on", port);
+app.listen(port, () => {
+  console.log('listening on port: ', port);
 })
 
 module.exports = app
